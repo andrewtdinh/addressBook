cd067172e7900bc66c35e3435a41d2de88eac14c
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-30 19:37:00 -0700

    config routing list

--- client/config/config.js
+++ client/config/config.js
@@ -8,8 +8,8 @@ angular.module('addressBook')
   .state('home', {url: '/', templateUrl: '/views/general/home.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
-  .state('albums', {url: '/albums', templateUrl: '/views/albums/albums.html', abstract: true})
-  .state('albums.list', {url: '', templateUrl: '/views/albums/albums-list.html', controller: 'AlbumsListCtrl'})
-  .state('albums.new', {url: '/new', templateUrl: '/views/albums/albums-new.html', controller: 'AlbumsNewCtrl'})
-  .state('albums.show', {url: '/{album}', templateUrl: '/views/albums/albums-show.html', controller: 'AlbumsShowCtrl'});
+  .state('contacts', {url: '/contacts', templateUrl: '/views/contacts/contacts.html', abstract: true})
+  .state('contacts.list', {url: '', templateUrl: '/views/contacts/contacts-list.html', controller: 'ContactsListCtrl'})
+  .state('contacts.new', {url: '/new', templateUrl: '/views/contacts/contacts-new.html', controller: 'ContactsNewCtrl'})
+  .state('contacts.show', {url: '/{name}/{bday}', templateUrl: '/views/contacts/contacts-show.html', controller: 'ContactsShowCtrl'});
 });
